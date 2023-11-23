200aded29dc22248932a7eddf1c104399e3d46c3
lukashronec <info@lukashronec.cz>
2023-11-23 20:57:16 +0100

    test browser

--- src/services/clubs/clubs.ts
+++ src/services/clubs/clubs.ts
@@ -45,7 +45,7 @@ const fetchMatches = async (app: Application) => {
         try {
           const response = await axios.get(clubUrl, {
             headers: {
-              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+              'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
               'Accept-Language': 'en-US,en;q=0.9',
               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
               'Accept-Encoding': 'gzip, deflate, br',
